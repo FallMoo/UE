be5124086853dd1a589175fcac5058d48a32dfea
WXL <weixiangli@zju.edu.cn>
2023-09-20 23:08:25 +0800

    first commit

---
+++ Source/c/cGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "cGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AcGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AcGameMode();
+};
+
+
+
